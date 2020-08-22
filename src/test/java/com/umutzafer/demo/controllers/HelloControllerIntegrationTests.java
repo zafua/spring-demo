ab5859c8c99837d8183cbb21190405a065ad086b
zafua <umutzafer@gmail.com>
2020-08-22 18:15:21 +0300

    Module1 bitti

---
+++ src/test/java/com/umutzafer/demo/controllers/HelloControllerIntegrationTests.java
@@ -0,0 +1,36 @@
+package com.umutzafer.demo.controllers;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerIntegrationTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testHelloWithoutName() throws Exception {
+        mvc.perform(get("/hello").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("user", "world"));
+    }
+
+    @Test
+    public void testHelloWithName() throws Exception {
+        mvc.perform(get("/hello").param("name", "Dolly").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("user", "Dolly"));
+    }
+}
